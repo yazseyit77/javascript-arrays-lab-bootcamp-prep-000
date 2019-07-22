b27b93bdece417ee67a87be3276160b4f8530d0e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-22 05:18:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function prependKitten(name){
   return [name, ...kittens];
 }
 function removeLastKitten(){
-  return  kittens.slice(0, kittens.length - 1);
+  return kittens.slice(0, kittens.length - 1);
 }
