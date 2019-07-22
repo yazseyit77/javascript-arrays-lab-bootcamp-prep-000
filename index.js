a3139512ea3eb2c835c78c1136211b681442444d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-22 05:09:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  kittens.appendKitten();
+  kittens.appendKitten(name);
   return kittens;
 }
